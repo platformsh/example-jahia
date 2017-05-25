2f2f4fefc3238145d07e5893cd4318afa856b1e3
Larry Garfield <larry@platform.sh>
2017-05-25 11:58:22 -0500

    Use a JSON file to define what plugins to install.

--- deploy.py
+++ deploy.py
@@ -1,7 +1,6 @@
 import json
 import base64
 import os
-from subprocess import call
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -10,34 +9,45 @@ DX_USERNAME = 'root'
 DX_PASSWORD = 'root'
 DX_RELATIONSHIP = 'jahia'
 
-platform_relationships = os.getenv('PLATFORM_RELATIONSHIPS')
-if platform_relationships:
-    relationships = json.loads(base64.b64decode(platform_relationships).decode('utf-8'))
-
-    # Extract the relationships that we want to expose to the application.
-    jahia = relationships[DX_RELATIONSHIP][0]
-
-    contextPath = ''
-
-    DX_REST_URL="http://{}:{}/modules/api/bundles/".format(
-        jahia['host'],
-        jahia['port'],
-    )
-
-    #env = dict(os.environ)
-    #install = env['JARS_INSTALL'].split()
-    #setup = env['JARS_SETUP'].split()
-    #start = env['JARS_START'].split()
-
-    # Make this dynamic, or more easily editable, or something.
-    filename = 'jahia-module/target/audit-trail-1.0-SNAPSHOT.jar'
-
+def send_install_request(filename):
     with open(filename, 'rb') as jar_handle:
-        #print "File open"
         files = {"bundle": jar_handle}
         data = {'start': 'true'}
-        #print "Sending request"
-        response = requests.post(DX_REST_URL, files=files, data=data, auth=HTTPBasicAuth(DX_USERNAME, DX_PASSWORD))
+        url = dx_rest_url()
+        response = requests.post(url, files=files, data=data, auth=HTTPBasicAuth(DX_USERNAME, DX_PASSWORD))
         response.raise_for_status()
-        #print "Sent request"
-        #print "Status: " + str(response.status_code)
+        print "Plugin {} installed and started successfully.".format(filename)
+
+def install_plugins(commands):
+    if 'install' in commands.keys():
+        for filename in commands['install']:
+            send_install_request(filename)
+
+def uninstall_plugins(commands):
+    pass
+#    if 'install' in commands.keys():
+#        for filename in commands['uninstall']:
+#            send_install_request(filename)
+
+def dx_rest_url():
+    platform_relationships = os.getenv('PLATFORM_RELATIONSHIPS')
+    if platform_relationships:
+        relationships = json.loads(base64.b64decode(platform_relationships).decode('utf-8'))
+
+        # Extract the relationships that we want to expose to the application.
+        jahia = relationships[DX_RELATIONSHIP][0]
+
+        url = "http://{}:{}/modules/api/bundles/".format(
+            jahia['host'],
+            jahia['port'],
+        )
+        return url
+
+def run():
+    with open('deploy.json', 'r') as json_file:
+        commands = json.load(json_file)
+
+    install_plugins(commands)
+    uninstall_plugins(commands)
+
+run()
